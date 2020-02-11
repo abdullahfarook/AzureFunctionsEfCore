680d0dc38a9b66486931708b693d196d170f770e
Abdullah Farooq <Abdullah.gb@hotmail.com>
2020-02-11 13:55:53 +0500

    add: service

---
+++ Qx.Services/Context.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Qx.Services
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public DateTime CreatedTime { get; set; } = DateTime.UtcNow;
+        public string TaskDescription { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions options)
+            : base(options)
+        { }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ Qx.Services/TodoService.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Qx.Services
+{
+    public class TodoService
+    {
+        private readonly TodoContext _todoContext;
+
+        public TodoService(TodoContext todoContext)
+        {
+            _todoContext = todoContext;
+        }
+
+        public Task<List<Todo>> GetAll()
+        {
+            return _todoContext.Todos.ToListAsync();
+        }
+        public async Task<Todo> Add(Todo item)
+        {
+             _todoContext.Todos.Add(item);
+             await _todoContext.SaveChangesAsync();
+             return item;
+        }
+    }
+}
